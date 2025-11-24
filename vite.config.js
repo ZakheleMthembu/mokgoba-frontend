28df020111e26a4845758dc4dcaef42198c96f7f
ZakheleMthembu <zikibellaprojects@gmail.com>
2025-11-24 17:25:08 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: process.env.PORT || 5173,
     host: true
   },
+
   preview: {
     port: process.env.PORT || 5173,
     host: true,
     allowedHosts: ['mokgoba-frontend.onrender.com']
   }
 })
+
